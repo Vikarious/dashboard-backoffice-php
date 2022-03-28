b45d333a6af2db4262cfc97b14fe63e69756bc54
Vikarious <fialaire.arnaud@free.fr>
2022-03-29 01:26:17 +0200

    asdf

--- view/users.view.php
+++ view/users.view.php
@@ -8,7 +8,7 @@
             <div class="card mb-4">
                 <div class="card-header">
                     <i class="fas fa-table me-1"></i>
-                    Blog Posts
+                    Users
                 </div>
                 <div class="card-body">
                     <table id="datatablesSimple">
